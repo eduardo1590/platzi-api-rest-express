5460cd94a7eaa551ca248fbabe7c55f87a11d292
Eduardo Rahn Garcia <eduardo1590@gmail.com>
2021-10-07 17:29:00 -0400

    primeras rutas express

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("My App")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi express server')
+});
+app.get('/nueva-ruta', (req, res) => {
+  res.json({
+    ruta: "nueva-ruta",
+    saludo: "Hola"
+  })
+});
+app.get('/productos', (req, res) => {
+  res.json({
+    nombre: "Bujia denso 3130",
+    precio: 25
+  })
+});
+
+app.listen(port, () =>{
+  console.log('Escuchando en: '+ port)
+});
